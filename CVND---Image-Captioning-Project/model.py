3074ecfa8f81eb446c38fa526bc3c83df6c6c0d7
robin.shie <robin.shie@outlook.com>
2018-05-27 11:23:05 +0800

    add cnvm image catption

--- CVND---Image-Captioning-Project/model.py
+++ CVND---Image-Captioning-Project/model.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-
-
+import torch.autograd as autograd
+from torch.nn.utils.rnn import pack_padded_sequence
+import torch.nn.functional as F
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
@@ -19,15 +20,42 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
-    def forward(self, features, captions):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+        self.vocab_size = vocab_size
+        #self.attn = nn.Linear(hidden_size*2,vocab_size)
+        #self.attn_combine = nn.Linear(hidden_size*2,hidden_size)
+    def init_weights(self):
+        """Initialize weights."""
+        self.embed.weight.data.uniform_(-0.1, 0.1)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
 
-    def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+    def forward(self, features, captions):
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        lengths = [len(cap) for cap in captions]
+        packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
+        hiddens, _ = self.lstm(packed)
+        outputs = self.linear(hiddens[0])
+        outputs = outputs.view(captions.shape[0],captions.shape[1],self.vocab_size)
+        return outputs
+    
+    def sample(self, features, states=None):
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(20):                                    # maximum sampling length
+            hiddens, states = self.lstm(inputs, states)        # (batch_size, 1, hidden_size), 
+            outputs = self.linear(hiddens.squeeze(1))          # (batch_size, vocab_size)
+            predicted = outputs.max(1)[1]
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)                       # (batch_size, 1, embed_size)
+        sampled_ids = torch.cat(sampled_ids, 1)                # (batch_size, 20)
+        return sampled_ids.squeeze()
